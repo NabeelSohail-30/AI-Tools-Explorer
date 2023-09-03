2efb8ee9272bce1dcf7f2b5d590165a712d67ca8
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-03 23:39:52 +0500

    add react router

--- client/myapp/src/index.js
+++ client/myapp/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ReactRouter from 'react-router';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ReactRouter.BrowserRouter>
+      <App />
+    </ReactRouter.BrowserRouter>
   </React.StrictMode>
 );
