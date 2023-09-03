1a4432d0710e48a7ea19a51f10bd8f6a3e2a46a1
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-03 23:40:10 +0500

    some changes

--- client/myapp/src/App.js
+++ client/myapp/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import './components/login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>My React App</h1>
-      </header>
+      <login />
     </div>
   );
 }
