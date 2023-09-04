606a370ebbf25d225c3f17ea8b7432dd78b951c2
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-04 20:58:59 +0500

    changes in register design

--- client/myapp/src/components/register.jsx
+++ client/myapp/src/components/register.jsx
@@ -27,6 +27,94 @@ export default function Register() {
     <ThemeProvider theme={defaultTheme}>
       <Grid container component="main" sx={{ height: "100vh" }}>
         <CssBaseline />
+        <Grid item xs={9} sm={7} md={4} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 4,
+              mx: 10,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Create New Account
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="Full Name"
+                name="name"
+                autoComplete="name"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="phone"
+                label="Phone Number"
+                name="phone"
+                autoComplete="phone"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="re_password"
+                label="Re Enter Password"
+                type="password"
+                id="re_password"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Register Now
+              </Button>
+              <Grid item>
+                <Grid item>
+                  <RouterLink to="/" variant="body2">
+                    {"Already have an account? Login"}
+                  </RouterLink>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Grid>
         <Grid
           item
           xs={false}
@@ -43,70 +131,6 @@ export default function Register() {
             backgroundPosition: "center",
           }}
         />
-        <Grid item xs={9} sm={7} md={4} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 18,
-              mx: 10,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Register
-            </Typography>
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 1 }}
-            >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              {/* <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              /> */}
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Register Now
-              </Button>
-              <Grid item>
-                <Grid item>
-                  <RouterLink to="/" variant="body2">
-                    {"already have an account? Login"}
-                  </RouterLink>
-                </Grid>
-              </Grid>
-            </Box>
-          </Box>
-        </Grid>
       </Grid>
     </ThemeProvider>
   );
