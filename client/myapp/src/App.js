f3bd28bd942ecb01b4f75a26c2696b7e199c35a8
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-04 20:58:40 +0500

    page not found route

--- client/myapp/src/App.js
+++ client/myapp/src/App.js
@@ -2,12 +2,15 @@ import { Routes, Route } from 'react-router-dom';
 
 import Login from './components/login';
 import Register from './components/register';
+import NotFound from './components/notfound';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
+
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
