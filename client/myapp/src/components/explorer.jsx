fca98b8a8dbfef2873b0431df206625d9710b58a
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-05 13:27:07 +0500

    explorer page

---
+++ client/myapp/src/components/explorer.jsx
@@ -0,0 +1,16 @@
+import Header from "./header";
+import FooterComponent from "./footer";
+import AItools from "./aitools";
+
+const Explorer = () => {
+  return (
+    <>
+      <Header />
+      <h1>Explorer</h1>
+      <AItools />
+      <FooterComponent />
+    </>
+  );
+};
+
+export default Explorer;
