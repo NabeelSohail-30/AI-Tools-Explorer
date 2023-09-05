9032088aa08846350938e06a326cb27275ff1f5d
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-05 14:49:47 +0500

    done for now

--- client/myapp/src/components/explorer.jsx
+++ client/myapp/src/components/explorer.jsx
@@ -1,12 +1,32 @@
+import React from "react";
+import { Typography, Box } from "@mui/material";
+
 import Header from "./header";
 import FooterComponent from "./footer";
 import AItools from "./aitools";
 
 const Explorer = () => {
+  const containerStyle = {
+    backgroundColor: "#333",
+    color: "white",
+    padding: "20px",
+    margin: "20px auto",
+    border: "1px solid #ccc",
+    borderRadius: "14px",
+    width: "50%",
+  };
+
   return (
     <>
       <Header />
-      <h1>Explorer</h1>
+      <Box
+        style={containerStyle}
+        display={"flex"}
+        justifyContent={"center"}
+        textAlign={"center"}
+      >
+        <Typography variant="h2">AI Tools Explorer</Typography>
+      </Box>
       <AItools />
       <FooterComponent />
     </>
