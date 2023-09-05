60a20a028a5f990076d2f4f908b5342a05f43a5b
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-05 13:28:32 +0500

    explorer route

--- client/myapp/src/App.js
+++ client/myapp/src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './components/login';
 import Register from './components/register';
 import NotFound from './components/notfound';
+import Explorer from './components/explorer';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/explorer" element={<Explorer />} />
 
       <Route path="*" element={<NotFound />} />
     </Routes>
