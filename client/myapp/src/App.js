4b694bf898685baaf315bb1f0cfdf9b6935b953e
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-05 20:07:11 +0500

    add tool route

--- client/myapp/src/App.js
+++ client/myapp/src/App.js
@@ -4,6 +4,7 @@ import Login from './components/login';
 import Register from './components/register';
 import NotFound from './components/notfound';
 import Explorer from './components/explorer';
+import AddAIToolForm from './components/addtool';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/explorer" element={<Explorer />} />
+      <Route path="/addtool" element={<AddAIToolForm />} />
+
 
       <Route path="*" element={<NotFound />} />
     </Routes>
