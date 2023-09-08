297688d61f8f9c52582a68ac22d25d7dfb570bc0
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-09 00:42:06 +0500

    server code done

--- server/index.mjs
+++ server/index.mjs
@@ -2,6 +2,8 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import toolsRoute from './routes/toolsRoute.js'
 
 dotenv.config()
 
@@ -10,9 +12,18 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    console.log(req)
+    res.status(200).send('AI Tools Explorer App')
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is listening on port ${process.env.PORT}`)
-});
+mongoose
+    .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
