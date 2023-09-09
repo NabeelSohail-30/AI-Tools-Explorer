d60ca367a2559825e8641786b87f7a4a8ffb1ba1
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-09 22:15:10 +0500

    working

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <Header></Header>
+      <div className="flex items-center justify-center h-screen">
+        <div className="text-center">
+          <h1 className="text-4xl font-semibold mb-4">About</h1>
+          <p className="text-lg">This is the About page.</p>
+        </div>
+      </div>
+      <Footer></Footer>
+    </>
+  );
 };
 
 export default About;
