78d5cf3481fed113a03ba58f88589ce4454e1909
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-09 21:43:50 +0500

    page not found

--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        <h1 className="text-4xl font-semibold mb-4">404 - Page Not Found</h1>
+        <p className="text-lg">The page you are looking for does not exist.</p>
+        <Link to={"/"} className="text-blue-500 hover:underline">
+          Go back to Home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default PageNotFound;
