6bcb4e4c9cacb8db97d23a0e368d8835186cce5a
Nabeel <nabeel.sohail2630@outlook.com>
2023-10-03 16:01:06 +0300

    adding modal feature

--- client/src/components/ToolCard.jsx
+++ client/src/components/ToolCard.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ToolCard = ({ tool }) => {
-  const redirectToTool = () => {
-    window.location.href = tool.link;
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
 
   return (
-    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="max-w-md p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         {tool.name}
       </h5>
@@ -14,30 +16,37 @@ const ToolCard = ({ tool }) => {
         {tool.category}
       </p>
       <p className="mb-1 text-sm font-medium text-gray-600 dark:text-gray-400">{`Owner: ${tool.owner}`}</p>
-      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-        {tool.description}
-      </p>
-      <button
-        onClick={redirectToTool}
-        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Go to Tool
-        <svg
-          className="w-3.5 h-3.5 ml-2"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 14 10"
+      <div className="mb-3">
+        <button
+          onClick={toggleModal}
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M1 5h12m0 0L9 1m4 4L9 9"
-          />
-        </svg>
-      </button>
+          View Details
+        </button>
+      </div>
+      {showModal && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="absolute inset-0 bg-gray-900 opacity-50"></div>
+          <div className="z-50 bg-white p-6 rounded-lg max-w-2xl overflow-y-auto">
+            <h2 className="text-2xl font-bold mb-4">{tool.name}</h2>
+            <p className="text-gray-600 mb-2">
+              <strong>Category:</strong> {tool.category}
+            </p>
+            <p className="text-gray-600 mb-2">
+              <strong>Owner:</strong> {tool.owner}
+            </p>
+            <p className="text-gray-600 mb-2">
+              <strong>Description:</strong> {tool.description}
+            </p>
+            <p
+              className="text-blue-700 hover:underline cursor-pointer"
+              onClick={toggleModal}
+            >
+              Close
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
