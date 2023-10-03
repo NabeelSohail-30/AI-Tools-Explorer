cf4d22922fb1a7f89fe5bfe41946cbb3ec86f9fe
Nabeel <nabeel.sohail2630@outlook.com>
2023-10-03 16:07:55 +0300

    unncessary text removed

--- client/src/pages/ExplorerPage.jsx
+++ client/src/pages/ExplorerPage.jsx
@@ -20,8 +20,6 @@ const Explorer = () => {
     <>
       <Header />
       <div className="container mx-auto mt-8 px-4 my-5">
-        <h1 className="text-4xl font-semibold mb-4 text-center">Explorer</h1>
-        <p className="text-lg text-center">This is the Explorer page.</p>
         <div className="mt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {tools.map((tool) => (
             <ToolCard key={tool._id} tool={tool} /> // Pass the tool data to ToolCard
