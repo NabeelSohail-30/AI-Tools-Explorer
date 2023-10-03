cc8a0e1ee7e6111776fa744655819fef25c83152
Nabeel <nabeel.sohail2630@outlook.com>
2023-10-03 12:45:39 +0300

    testing the tool component

--- client/src/pages/ExplorerPage.jsx
+++ client/src/pages/ExplorerPage.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import ToolCard from "../components/ToolCard";
+// import { useState } from "react";
 
 const Explorer = () => {
+  // const [tools, setTools] = useState([]);
+  
+  const tool = {
+    _id: "1",
+    name: "Tool Name",
+    description: "Tool Description",
+  };
+
   return (
     <>
       <Header></Header>
@@ -11,6 +21,7 @@ const Explorer = () => {
           <h1 className="text-4xl font-semibold mb-4">Explorer</h1>
           <p className="text-lg">This is the Explorer page.</p>
         </div>
+        <ToolCard key={tool._id} tool={tool} />
       </div>
       <Footer></Footer>
     </>
