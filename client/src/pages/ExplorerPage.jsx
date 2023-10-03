9fa90afd0af56c3d16eafd5593ae168a42d5f71f
Nabeel <nabeel.sohail2630@outlook.com>
2023-10-03 15:58:08 +0300

    working on the layout

--- client/src/pages/ExplorerPage.jsx
+++ client/src/pages/ExplorerPage.jsx
@@ -1,6 +1,4 @@
-import React from "react";
-import { useState, useEffect } from "react";
-
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import ToolCard from "../components/ToolCard";
@@ -20,17 +18,17 @@ const Explorer = () => {
 
   return (
     <>
-      <Header></Header>
-      <div className="flex items-center justify-center h-screen">
-        <div className="text-center">
-          <h1 className="text-4xl font-semibold mb-4">Explorer</h1>
-          <p className="text-lg">This is the Explorer page.</p>
+      <Header />
+      <div className="container mx-auto mt-8 px-4 my-5">
+        <h1 className="text-4xl font-semibold mb-4 text-center">Explorer</h1>
+        <p className="text-lg text-center">This is the Explorer page.</p>
+        <div className="mt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {tools.map((tool) => (
+            <ToolCard key={tool._id} tool={tool} /> // Pass the tool data to ToolCard
+          ))}
         </div>
       </div>
-      {tools.map((tool) => (
-        <ToolCard key={tool._id} tool={tool} /> // Pass the tool data to ToolCard
-      ))}{" "}
-      <Footer></Footer>
+      <Footer />
     </>
   );
 };
